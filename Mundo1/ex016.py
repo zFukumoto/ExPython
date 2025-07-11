8927a5eb2b16ec6575a156a57de9de2ba5c3a1f3
Gabriel Fukumoto <162069059+zFukumoto@users.noreply.github.com>
2025-07-11 19:51:35 -0300

    Add files via upload

---
+++ Mundo1/ex016.py
@@ -0,0 +1,5 @@
+# Crie um programa que leia um número Real qualquer pelo teclado e mostre na
+# tela a sua porção inteira.
+from math import trunc
+num = float(input('Digite um número Real: '))
+print(f'O número {num} tem a parte inteira {num}')
